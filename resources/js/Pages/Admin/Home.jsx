8f86e6c83c211715a847af5849d4283ed357f892
Amirul Azmi <amirullazmi0@gmail.com>
2023-03-01 21:18:18 +0700

    gas

--- resources/js/Pages/Admin/Home.jsx
+++ resources/js/Pages/Admin/Home.jsx
@@ -7,23 +7,23 @@ import { Head } from '@inertiajs/react';
 import React, { useEffect, useState } from 'react';
 import io from "socket.io-client";
 
-const SOCKET_SERVER_URL = 'http://127.0.0.1/8080';
+// const SOCKET_SERVER_URL = 'http://127.0.0.1/8080';
 
 
 
 export default function Home(props) {
-    const [message, setMessage] = useState('');
-    useEffect(() => {
-        const socket = io.connect(SOCKET_SERVER_URL);
+    const [message, setMessage] = useState('asdasd');
+    // useEffect(() => {
+    //     const socket = io.connect(SOCKET_SERVER_URL);
 
-        socket.on('message', (message) => {
-            setMessage(message);
-        });
+    //     socket.on('message', (message) => {
+    //         setMessage(message);
+    //     });
 
-        // return () => {
-        //     socket.disconnect();
-        // };
-    }, []);
+    //     // return () => {
+    //     //     socket.disconnect();
+    //     // };
+    // }, []);
     return (
         <div className='bg-body'>
             <Head title={props.title} />
